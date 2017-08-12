7688c803ec415365338fdfd6e4d1f3f4771f640c
Or Keren <dude2110@gmail.com>
2017-08-12 12:31:13 +0300

    Added button that opens simple modal

--- app/index.js
+++ app/index.js
@@ -1,12 +1,38 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import Modal from "react-modal";
+import React from 'react';
+import ReactDOM from 'react-dom';
 require('./index.css');
 
 class App extends React.Component{
-  render(){
+    constructor () {
+        super();
+        this.state = {
+            showModal: false
+        };
+
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    handleOpenModal () {
+        this.setState({ showModal: true });
+    }
+
+    handleCloseModal () {
+        this.setState({ showModal: false });
+    }
+
+    render(){
     return (
       <div>
-        Hello World!
+          <button onClick={this.handleOpenModal}>Trigger Modal</button>
+          <Modal
+              isOpen={this.state.showModal}
+              contentLabel="Minimal Modal Example">
+              <text>Enter Text Here:</text>
+              <button onClick={this.handleCloseModal}>Close Modal</button>
+          </Modal>
+
       </div>)
   }
 }
