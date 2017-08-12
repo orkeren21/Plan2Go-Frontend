2e3cc71d6d9221982d918fcef24dc0caa65fae6e
Or Keren <dude2110@gmail.com>
2017-08-12 15:08:30 +0300

    Modal opens with relevant fields

--- app/index.js
+++ app/index.js
@@ -29,12 +29,83 @@ class App extends React.Component{
           <Modal
               isOpen={this.state.showModal}
               contentLabel="Minimal Modal Example">
-              <text>Enter Text Here:</text>
-              <button onClick={this.handleCloseModal}>Close Modal</button>
+              <EventForm />
+              <br/>
+              <button onClick={this.handleCloseModal}>Cancel</button>
           </Modal>
 
       </div>)
   }
+}
+
+
+class EventForm extends React.Component{
+    startDate;
+    endDate;
+    eventStartTime;
+    eventEndTime;
+    eventTitle;
+    eventDescription;
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const name = target.name;
+
+        this.setState({
+            [name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        this.setState({ showModal: false });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Start Date:
+                    <input name="startDate" type="text" value={this.state.startDate} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    End Date:
+                    <input name="endDate" type="text" value={this.state.endDate} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    Start Time:
+                    <input name="eventStartTime" type="text" value={this.state.eventStartTime} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    End Time:
+                    <input name="eventEndTime" type="text" value={this.state.eventEndTime} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    Title:
+                    <input name="eventTitle" type="text" value={this.state.eventTitle} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    Description:
+                    <textarea name="eventDescription" value={this.state.eventDescription} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <br/>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
 }
 
 ReactDOM.render(
